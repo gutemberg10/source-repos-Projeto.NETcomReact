a1551f69952accd5ce67502c5c968544c4b60453
Gutemberg Nunes <g.a.do.nascimento@avanade.com>
2022-05-31 16:14:29 -0300

    Meu projeto

---
+++ AlunosApi/AlunosApi/Context/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlunosApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Maria da Penha",
+                    Email = "mariapenha@gmail.com",
+                    Idade = 65
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Manuel Bueno",
+                    Email = "manuel@gmail.com",
+                    Idade = 32
+                }
+                );
+        }
+    }
+}
---
+++ AlunosApi/AlunosApi/Migrations/20220516124731_PopularTabela.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlunosApi.Migrations
+{
+    public partial class PopularTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 1, "mariapenha@gmail.com", 65, "Maria da Penha" });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 2, "manuel@gmail.com", 32, "Manuel Bueno" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
